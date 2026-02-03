8158a69d51c3753040498c06193b2468e342b1a0
Pierre Jeanjean <pierre.jeanjean@xwiki.com>
2026-02-03 15:10:06 +0100

    CRISTALNC-20: Vue should not be bundled

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,41 @@
-import { createAppConfig } from "@nextcloud/vite-config";
-import { join, resolve } from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
-export default createAppConfig(
-  {
-    main: resolve(join("src", "main.ts")),
+export default defineConfig({
+  plugins: [vue()],
+  define: {
+    'process.env': {},
   },
-  {
-    createEmptyCSSEntryPoints: true,
-    extractLicenseInformation: true,
-    thirdPartyLicense: false,
-  }
-);
+  resolve: {
+    alias: {
+      // FORCE absolute path to the bundler version of Vue.
+      // This is required for "import Vue" to work correctly.
+      vue: resolve(__dirname, "node_modules/vue/dist/vue.esm-bundler.js"),
+    },
+    dedupe: ["vue"],
+  },
+  build: {
+    outDir: ".",
+    lib: {
+      entry: resolve(__dirname, "src/main.js"),
+      name: "cristal",
+      formats: ["iife"],
+      fileName: () => "js/cristal-main.js",
+    },
+    
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.names.length > 0 && assetInfo.names[0].endsWith(".css")) {
+            return "css/cristal-main.css";
+          }
+          return "css/[name][extname]";
+        },
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});
